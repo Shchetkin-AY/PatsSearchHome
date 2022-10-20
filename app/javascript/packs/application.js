3898d42bdcdbbb64a82272acab54fc043d9ebceb
Shchetkin.A.Y <shchetkin.a.y@gmail.com>
2022-10-20 23:04:31 +0700

    Integrated fonts and styles

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,9 +2,10 @@
 
 
 import "@hotwired/turbo-rails"
-import "controllers"
+import "app/javascript/controllers"
 import "bootstrap";
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels").start
+import("sb-admin-2")
